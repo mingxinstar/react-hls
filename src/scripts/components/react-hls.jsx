ec4c249f65c60268782cadf60356e5fd3e23cd6f
mingxin.huang <mingxin.huang@2339.com>
2017-01-11 23:24:20 +0800

    Add hls.js base playing feature to the component

--- src/scripts/components/react-hls.jsx
+++ src/scripts/components/react-hls.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { PropTypes } from 'react';
-import hls from 'hls.js';
+import Hls from 'hls.js';
 
 class ReactHls extends React.Component {
     constructor (props) {
@@ -12,16 +12,37 @@ class ReactHls extends React.Component {
         };
     }
 
-    componentDidMount () {
+    componentDidUpdate () {
+        console.log('componentDidUpdate');
 
+        this._initPlayer();
+    }
+
+    componentDidMount () {
+        this._initPlayer();
+    }
+
+    _initPlayer () {
+        let { url } = this.props;
+        let { playerId } = this.state;
+        let $video = document.getElementById(`react-hls-${playerId}`);
+        let hls = new Hls();
+
+        hls.loadSource(url);
+        hls.attachMedia($video);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+            $video.play();
+        });
     }
 
     render () {
+        console.log('render video');
+
         let { playerId } = this.state;
 
         return (
             <div key={playerId} className="player-area">
-                <video className="hls-player" id={`react-hls-${playerId}`}></video>
+                <video className="hls-player" id={`react-hls-${playerId}`} controls></video>
             </div>
         )
     }
