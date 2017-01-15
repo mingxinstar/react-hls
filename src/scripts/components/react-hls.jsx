0aeaeb32c914711bf31f2c8c98bd10061fa49d4e
mingxin.huang <mingxin.huang@2339.com>
2017-01-15 22:27:47 +0800

    Test for publish to npm

--- src/scripts/components/react-hls.jsx
+++ src/scripts/components/react-hls.jsx
@@ -23,33 +23,49 @@ class ReactHls extends React.Component {
     }
 
     _initPlayer () {
-        let { url } = this.props;
+        if (this.hls) {
+            this.hls.destroy();
+        }
+
+        let { url, autoplay, hlsConfig, controls } = this.props;
         let { playerId } = this.state;
         let $video = document.getElementById(`react-hls-${playerId}`);
-        let hls = new Hls();
+        let hls = new Hls(hlsConfig);
 
         hls.loadSource(url);
         hls.attachMedia($video);
         hls.on(Hls.Events.MANIFEST_PARSED, () => {
-            $video.play();
+            if (autoplay) {
+                $video.play();
+            }
         });
+
+        this.hls = hls;
     }
 
     render () {
-        console.log('render video');
-
         let { playerId } = this.state;
+        let { controls } = this.props;
 
         return (
             <div key={playerId} className="player-area">
-                <video className="hls-player" id={`react-hls-${playerId}`} controls></video>
+                <video className="hls-player" id={`react-hls-${playerId}`} controls={controls}></video>
             </div>
         )
     }
 }
 
 ReactHls.propTypes = {
-    url : PropTypes.string.isRequired
+    url : PropTypes.string.isRequired,
+    autoplay : PropTypes.bool,
+    hlsConfig : PropTypes.object, //https://github.com/dailymotion/hls.js/blob/master/API.md#fine-tuning
+    controls : PropTypes.bool
+}
+
+ReactHls.defaultProps = {
+    autoplay : true,
+    hlsConfig : {},
+    controls : true
 }
 
 export default ReactHls;
