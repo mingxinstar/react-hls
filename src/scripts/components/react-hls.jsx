485abf921efeb6794dde598679495874cece8562
Ernesto Jimenez <ejimenez.mit@gmail.com>
2018-10-13 20:37:09 -0600

    allow disableInitOnUpdate to be controlled after mounting

--- src/scripts/components/react-hls.jsx
+++ src/scripts/components/react-hls.jsx
@@ -11,12 +11,12 @@ class ReactHls extends React.Component {
         this.state = {
             playerId : Date.now()
         };
-        this.disableInitOnUpdate = props.disableInitOnUpdate;
+
         this.hls = null;
     }
 
     componentDidUpdate () {
-        if (!this.disableInitOnUpdate) this._initPlayer();
+        if (!this.props.disableInitOnUpdate) this._initPlayer();
     }
 
     componentDidMount () {
